ff542a6e1d1bc23f12c08fad3a68b15b7813fcd6
caiopicciarelli2 <caio.psilva@sptech.school>
2026-04-07 19:36:42 -0300

    Arrumando padronizações no simulador

--- Site/assets/js/script.js
+++ Site/assets/js/script.js
@@ -1,3 +1,7 @@
 function redirecionamento_login() {
     window.location.href = "../Cadastro_Login/login.html";
 }
+
+function redirecionamento_simulador(){
+    window.location.href='../Simulador_financeiro/Simulador.html'
+}
